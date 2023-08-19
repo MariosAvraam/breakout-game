aefa0fa77870f5a4a8ca5bd6e7fda0ea56f53be3
Marios <marios_2510@hotmail.com>
2023-08-19 15:25:35 +0300

    Refactor paddle.py.

--- paddle.py
+++ paddle.py
@@ -1,21 +1,35 @@
 import turtle
 
 class Paddle:
+    """
+    Represents the paddle in the Breakout game.
+    Handles the paddle's movements and its visual representation.
+    """
+    
     def __init__(self):
+        """
+        Initialize the paddle with default attributes and position it at the bottom center of the screen.
+        """
         self.paddle = turtle.Turtle()
-        self.paddle.speed(0)
-        self.paddle.shape("square")
+        self.paddle.speed(0)  # Maximum drawing speed
+        self.paddle.shape("square")  # Base shape for the paddle
         self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
+        self.paddle.shapesize(stretch_wid=1, stretch_len=5)  # Adjust the paddle size
         self.paddle.penup()
-        self.paddle.goto(0, -200)
+        self.paddle.goto(0, -200)  # Starting position of the paddle
         
     def move_left(self):
+        """
+        Move the paddle to the left by a fixed amount.
+        """
         x = self.paddle.xcor()
-        x -= 20
+        x -= 20  # Amount to move left
         self.paddle.setx(x)
 
     def move_right(self):
+        """
+        Move the paddle to the right by a fixed amount.
+        """
         x = self.paddle.xcor()
-        x += 20
+        x += 20  # Amount to move right
         self.paddle.setx(x)
